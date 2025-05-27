a854195fdba9eadf07998d73ebdab38103556dd5
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-05-27 07:13:27 -0600

    Debugging file added

--- lua/user/debug.lua
+++
@@ -1,8 +0,0 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-  callback = function()
-    vim.defer_fn(function()
-      print("AFTER colorscheme:", vim.inspect(vim.api.nvim_get_hl(0, { name = "Normal" })))
-    end, 100)
-  end
-})
-
