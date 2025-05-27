2aa5870743d67bca5a120bb679656d55b966e7ff
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-05-27 07:16:33 -0600

    Fixes

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -1,8 +1,14 @@
-print("Sourcing lua/user/init.lua")
+-- User configuration
 
+-- Most autocommands and command groups
 require("user.autocmds")
+-- Some sweet keybinds
 require("user.remaps")
+-- Global function definitions
 require("user.globals")
+-- All those options that make Vim useful
 require("user.set")
-
+-- UI changes
+require("user.ui")
+-- Debugging commands for future use (selectively enable as desired)
 require("user.debug")
