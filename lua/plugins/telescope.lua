37e3f83cdbb63bc86da48a9e50fd359576cf95b3
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-05-31 01:39:43 -0600

    Added telescope bindings

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,3 @@
--- TODO: Need to add the following keybinds:
---
---  - Find symbols in current buffer and entire codebase
---  - Find strings in current buffer and entire codebase
---  - Go to definitions
---  - Go to reference
---  - Show diagnostics for current file and entire codebase
---
 return {
     {
         'nvim-telescope/telescope-fzf-native.nvim',
@@ -23,6 +15,34 @@ return {
 
         config = function()
             require("telescope").load_extension("fzf")
+
+            local builtin = require("telescope.builtin")
+            -- File pickers
+            vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Lists all files, respects .gitignore" })
+            vim.keymap.set("n", "<leader>pg", builtin.git_files, { desc = "Lists only explicitly tracked files, respects .gitignore" })
+            vim.keymap.set("n", "<leader>ps", function()
+                builtin.grep_string({ search = vim.fn.input("Grep > ") })
+            end)
+
+            -- LSP specific pickers
+            vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols, { desc = "List symbols in current buffer" })
+            vim.keymap.set("n", "<leader>Ls", builtin.lsp_document_symbols, { desc = "List symbols across project" })
+            vim.keymap.set("n", "<leader>lr", builtin.lsp_references, { desc = "List all references under cursor" })
+
+            -- Vim pickers
+            vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Show open buffers" })
+
+            -- Stolen from kickstart....not sure if I'll use them
+            vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+            vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+            vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+            vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+            vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+            vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+            vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+            vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+
         end
     }
+
 }
