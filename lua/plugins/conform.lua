8c401632031271e6c5ff0b88071a368926fcc7d9
George Castillo <4260836+gmcastil@users.noreply.github.com>
2025-08-03 05:55:30 -0600

    Disable auto formatting in C and C++ files

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -24,23 +24,45 @@ return {
     ---@module "conform"
     ---@type conform.setupOpts
     opts = {
+        notify_on_error = true,
+        -- Disable "format_on_save lsp_fallback" for languages without good
+        -- coding style standards
+        format_on_save = function(bufnr)
+            -- Disable autoformat on certain filetypes
+            local ignore_filetypes = { "c", "cpp" }
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+                return
+            end
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                return
+            end
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match("/node_modules/") then
+                return
+            end
+            return { timeout_ms = 500, lsp_format = "fallback" }
+        end,
         -- Define your formatters
         formatters_by_ft = {
-            -- lua = { "stylua" },
+            c = { "clang_format" },
+            lua = { "stylua" },
             python = { "black" },
-            -- markedown = { "prettierd" },
-            -- javascript = { "prettierd", "prettier", stop_after_first = true },
+            markdown = { "prettierd" },
+            javascript = { "prettierd", "prettier", stop_after_first = true },
         },
         -- Set default options
         default_format_opts = {
             lsp_format = "fallback",
         },
-        -- Set up format-on-save
-        format_on_save = { timeout_ms = 500 },
         -- Customize formatters
         formatters = {
+            clang_format = {
+                prepend_args = { '--fallback-style=LLVM' },
+            },
             shfmt = {
-                prepend_args = { "-i", "2" },
+                prepend_args = { "-i", "4" },
             },
         },
     },
