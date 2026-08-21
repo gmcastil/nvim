dc60f3450c8e31130f581718ae7ac0ade934690d
George Castillo <george.castillo@yorkspacesystems.com>
2026-08-21 16:23:09 -0600

    Add some missing keybinds

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -39,6 +39,32 @@ return {
 		vim.keymap.set("n", "<leader>pl", builtin.live_grep,
 		{ desc = "Search for a string and get results live as you type, respects .gitignore" })
 
-	end,
+		-- LSP specific pickers
+		vim.keymap.set("n", "<leader>ls", builtin.lsp_document_symbols,
+		{ desc = "Telescope: List symbols in current buffer" })
+		vim.keymap.set("n", "<leader>lS", builtin.lsp_workspace_symbols,
+		{ desc = "Telescope: List symbols across project" })
+		vim.keymap.set("n", "<leader>ld", builtin.lsp_definitions,
+		{ desc = "Telescope: List symbol definitions" })
+		vim.keymap.set("n", "<leader>lr", builtin.lsp_references,
+		{ desc = "Telescope: List symol references" })
+		vim.keymap.set("n", "<leader>li", builtin.lsp_implementations,
+		{ desc = "Telescope: List symbol implementations" })
+		vim.keymap.set("n", "<leader>lt", builtin.lsp_type_definitions,
+		{ desc = "Telescope: List type definitions" })
+
+		-- Vim pickers
+		vim.keymap.set("n", "<leader>b", builtin.buffers, { desc = "Show open buffers" })
+
+		-- Stolen from kickstart....not sure if I'll use them
+		vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+		vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+		vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+		vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+		vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+		vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+    end,
 }
 
